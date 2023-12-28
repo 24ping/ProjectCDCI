8b7cfc3693f7d028c87b1c536280f50c17a6b684
Hamza <zaherhamza98@gmail.com>
2023-12-28 17:04:30 +0100

    fixed the codeexample.py error

--- codeExample.py
+++ codeExample.py
@@ -5,7 +5,3 @@ on the stage
 
 # Before Source Stage
 print("This is the before stage")
-
-# TODO : dont forget to remove this line
-# BUG: the button is not working correctly
-# FIXME: this code shoulde reinterpreted
