678fdaa73725d25512bfde170f55f189e9e02257
Hamza <zaherhamza98@gmail.com>
2023-12-28 16:50:08 +0100

    spelling mistakes

--- codeExample.py
+++ codeExample.py
@@ -5,3 +5,7 @@ on the stage
 
 # Before Source Stage
 print("This is the before stage")
+
+# TODO : dont forget to remove this line
+# BUG: the button is not working correctly
+# FIXME: this code shoulde reinterpreted
