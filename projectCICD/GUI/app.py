d63e44fbdbf37acdb6f9244c1b08699786aa555f
Hamza <zaherhamza98@gmail.com>
2024-03-22 15:22:14 +0000

    we are changing the programs

--- projectCICD/GUI/app.py
+++ projectCICD/GUI/app.py
@@ -2,9 +2,23 @@
 
 """
 
+from typing import Tuple
 import customtkinter
-# import os
+import os
 from PIL import Image
+
+
+class AppLogos(customtkinter.CTkImage):
+    def __init__(self):
+        super().__init__()
+        # Finding the path of the logos
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        concept_cicd_dir = os.path.abspath(os.path.join(current_path, os.pardir))
+        image_path = os.path.join(concept_cicd_dir, "data", "Images", "Logos")
+        path_to_images = Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png"))
+        # initializing images
+        self.logo_image = path_to_images
+        self.logo_image.size(26, 26)
 
 
 class App(customtkinter.CTk):
@@ -18,4 +32,13 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        # this a test to track my commits
+        # new frame for testing images
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
+
+        # new images objects
+        self.all_logos = AppLogos()
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  Image Example", image=self.all_logos.logo_image,
+                                                             compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
