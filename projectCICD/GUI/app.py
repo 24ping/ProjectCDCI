5eaa16d3ce7c7ce157c5379d5b460623d0d7f3a8
Hamza <zaherhamza98@gmail.com>
2024-03-22 01:27:55 +0000

    Some comments about the parameters of the class App

--- projectCICD/GUI/app.py
+++ projectCICD/GUI/app.py
@@ -6,12 +6,14 @@ import customtkinter
 # import os
 from PIL import Image
 
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
+        # Set the name of the window
         self.title("image_example.py")
-        self.resizable(width=True,height=True)
+        self.resizable(width=True, height=True)
 
+        # Set the layout of the window (1x2)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
