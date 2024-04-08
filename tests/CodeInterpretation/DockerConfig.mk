aa52b9339afccc27341ef0381296a025d785d78a
Hamza <zaherhamza98@gmail.com>
2024-04-08 03:05:31 +0000

    add the a new target to run test in make

--- tests/CodeInterpretation/DockerConfig.mk
+++ tests/CodeInterpretation/DockerConfig.mk
@@ -3,13 +3,15 @@
 Dir := $(shell pwd)
 Code_dir := $(subst tests/CodeInterpretation,projectCICD/AESClone,$(Dir))
 Docker_dir := $(subst tests/CodeInterpretation,docker,$(Dir))
+Test_dir := $(subst CodeInterpretation,unit,$(Dir))
 Code_name := main.py
+Test_code := test_functions.py
 Copy_code = $(shell cd $(Code_dir); cp $(Code_name) $(Docker_dir))
 
 
 
 # Adding the source code to the docker folder configuration
-docker-module: run-main-py clean code-to-docker
+docker-module: run-main-py test-main-py clean code-to-docker
 	@echo "*** updating the docker folder configuration........ 	***"
 
 
@@ -20,6 +22,15 @@ run-main-py:
 	@ cd $(Code_dir); \
 	python3.9 $(Code_name)
 	@echo "\n***	the code has successfully ran.......	***\n"
+
+# Running the test
+test-main-py:
+	@echo "\n"
+	@echo "***	testing the $(Test_code) ......	***\n"
+	@ cd $(Test_dir); \
+	python3.9 $(Test_code)
+	@echo "\n***	the test has successfully ran.......	***\n"
+
 
 # Cleaning previous versions of the source code
 clean:
